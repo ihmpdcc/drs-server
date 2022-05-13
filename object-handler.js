f9f8712b2022762859a8deeaef2e62616fca1623
Victor <victor73@users.noreply.github.com>
2022-05-13 19:04:32 -0400

    Changes to support interoperability with Cavatica mainly focused on the use of access_ids. Also some jsdoc commenting in various modules.

--- object-handler.js
+++ object-handler.js
@@ -11,7 +11,7 @@ const ObjectRetrieve = require('object-retrieve');
 let retriever = null;
 
 exports.init = function(emitter) {
-    logger.debug('In ' + path.basename(__filename) + ' init().');
+    logger.debug('In {} init().'.format(path.basename(__filename)));
 
     retriever = ObjectRetrieve.get_instance(config);
 
@@ -26,10 +26,55 @@ exports.init = function(emitter) {
 
 exports.get_object_access_url = function(request, response) {
     logger.debug('In get_object.');
-    var obj_id = request.params.object_id;
-    var access_id = request.params.access_id;
 
-    drs_utils.send_error(response, 'No access id is needed for HMP data.', 405);
+    let obj_id = request.params.object_id;
+    let full_given_access_id = request.params.access_id;
+
+    let expand = false;
+
+    retriever.build_object(obj_id, expand, function(data, err) {
+        if (err) {
+            drs_utils.send_error(response, 'An error occurred.', 500);
+        } else {
+            if (_.isArray(data) && data.length === 0) {
+                drs_utils.send_error(response, 'No DRS object with that id found.', 404);
+            } else {
+                // Compare the given access_id, to the one we have computed...
+                //
+                // The full access id will contain a hyphen followed by a number. These
+                // are used to disambiguate access_ids when there is more than 1 access_url.
+                // They are essentially numbered. So we split that part off and only
+                // compare the prefix portion.
+                let access_id = drs_utils.generate_access_id(obj_id);
+                let full_array = full_given_access_id.split('-');
+                let access_id_number = parseInt(full_array[1], 10);
+                let given_access_id = full_array[0];
+
+                if (given_access_id !== access_id) {
+                    logger.info('Access ID provided is invalid. Given {} != {}.'.format(
+                        given_access_id, access_id
+                    ));
+
+                    drs_utils.send_error(response, 'Access ID is invalid.', 403);
+                } else {
+                    let access_methods = data['access_methods'];
+
+                    if ((access_id_number <= 0) || (access_id_number > access_methods.length)) {
+                        logger.info('Access method index given is invalid or out of range.');
+                        drs_utils.send_error(response, 'Access method index is invalid.', 403);
+                    } else {
+                        let access_method = access_methods[access_id_number - 1];
+                        let access_url = access_method['access_url'];
+                        response.json(access_url);
+                    }
+                }
+            }
+
+            return;
+        }
+
+        return;
+    });
 };
 
 exports.get_object = function(request, response) {
@@ -59,14 +104,21 @@ exports.get_object = function(request, response) {
 // health of a backend target service, as in the case of running the service in
 // a cloud provider such as AWS.
 exports.get_status = function(request, response) {
-    let status = 'OK';
+    let ok = 'OK';
+    let not_ok = 'NOT_OK';
 
     let con = retriever.get_connection();
 
     if (con.state === 'authenticated') {
-        response.status(200).send(status);
+        con.ping(function(err) {
+            if (err) {
+                response.status(500).send(not_ok);
+            } else {
+                response.status(200).send(ok);
+            }
+        });
     } else {
-        response.status(500).send('NOT_OK');
+        response.status(500).send(not_ok);
     }
 };
 
